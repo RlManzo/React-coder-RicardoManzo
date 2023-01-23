fa33d2b1b2d25d39db498d3e24a1b3e6cc34f953
Ricardo Manzo <ricardojuliomanzo@gmail.com>
2023-01-23 12:56:13 -0300

    PrimerPreEntrega+ManzoRicardo

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
         <>
              <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Navbar</a>
+          <a className="navbar-brand btn-success" href="#">Choque Sublimables</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
           </button>
