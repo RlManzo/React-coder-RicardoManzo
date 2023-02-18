fdb5a566156ffa583aa94dd60f59efd000460b8f
Ricardo Manzo <ricardojuliomanzo@gmail.com>
2023-02-18 17:02:03 -0300

    antes de firebase

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -6,9 +6,9 @@ const Item = ({item}) => {
     <div className="card cardStyles" style={{width: '18rem'}}>
         <img src={`../ImgProducts/${item.img}`} className="card-img-top imgCard" alt="..." />
         <div className="card-body">
-          <h5 className="card-title">{item.nombre} </h5>
-          <p className="card-text">Marca: {item.marca} </p>
-          <p className="card-text">$ {new Intl.NumberFormat('de-DE').format(item.precio)} </p>
+          <h5 className="card-title titleColor">{item.nombre} </h5>
+          <p className="card-text titleColor1">Marca: {item.marca} </p>
+          <p className="card-text titleColor1">$ {new Intl.NumberFormat('de-DE').format(item.precio)} </p>
           <button className="btn btn-primary"><Link className="nav-link" to={`./item/${item.id}`}>Ver Producto</Link></button>
         </div>
     </div>
---
+++ src/firebase/firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB8_NsCbQyL8lPgaVzIsd-veBdCN85nT3I",
+  authDomain: "react-proyecto-coder-8aa7f.firebaseapp.com",
+  projectId: "react-proyecto-coder-8aa7f",
+  storageBucket: "react-proyecto-coder-8aa7f.appspot.com",
+  messagingSenderId: "14029244718",
+  appId: "1:14029244718:web:0893e3b3bc32871b8712bf"
+};
+
+
+const app = initializeApp(firebaseConfig);
